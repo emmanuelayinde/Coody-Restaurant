90ecb9c9598461a18f343d5bfb7b934d40a09ede
Engr SoluTion <emmanuelishola7@gmail.com>
2024-08-23 14:35:12 +0100

    add openAPI swagger docs

--- go-server/main.go
+++ go-server/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/emmanuelayinde/coody-restaurant/db"
 	"github.com/emmanuelayinde/coody-restaurant/routes"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -12,5 +14,13 @@ func main() {
 	v1Router := router.Group("/api/v1")
 	routes.SetupRoutes(v1Router)
 	db.OpenCollection(db.Client, "users")
+
+	v1Router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello, World!"})
+	})
+	v1Router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "ROUTE_NOT_FOUND", "message": "Route not found"})
+	})
 	router.Run(":" + config.AppConfig.PORT)
 }
